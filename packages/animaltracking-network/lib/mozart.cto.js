ba0dbdaae6d1043975d7c4e7ce89628bbdbeda38
Liam Grace <liamgrace.896@gmail.com>
2017-03-16 16:29:02 +0000

    Added SetupDemo transaction

--- packages/animaltracking-network/lib/mozart.cto.js
+++ packages/animaltracking-network/lib/mozart.cto.js
@@ -32,25 +32,30 @@ function onAnimalMovementDeparture(movementDeparture) {
   movementDeparture.animal.movementStatus = 'IN_TRANSIT';
 
      // save the animal
-  var ar = getAssetRegistry('com.ibm.concerto.mozart.Animal');
-  ar.update(movementDeparture.animal);
-
-     // add the animal to the incoming animals of the
-     // destination business
-  if (movementDeparture.to.incomingAnimals) {
-    movementDeparture.to.incomingAnimals.push(movementDeparture.animal);
-  } else {
-    movementDeparture.to.incomingAnimals = [movementDeparture.animal];
-  }
-
-     // save the business
-  var br = getAssetRegistry('com.ibm.concerto.mozart.Business');
-  br.update(movementDeparture.to);
+  return getAssetRegistry('com.ibm.concerto.mozart.Animal')
+  .then(function(ar) {
+    return ar.update(movementDeparture.animal);
+  })
+  .then(function() {
+    // add the animal to the incoming animals of the
+    // destination business
+    if (movementDeparture.to.incomingAnimals) {
+      movementDeparture.to.incomingAnimals.push(movementDeparture.animal);
+    } else {
+      movementDeparture.to.incomingAnimals = [movementDeparture.animal];
+    }
+    
+      // save the business
+     return getAssetRegistry('com.ibm.concerto.mozart.Business')
+  })
+  .then(function(br) {
+    return br.update(movementDeparture.to);
+  });
 }
 
 /**
  *
- * @param {com.ibm.concerto.mozart.AnimalMovementArrival} movementDeparture
+ * @param {com.ibm.concerto.mozart.AnimalMovementArrival} movementArrival
  * @transaction
  */
 function onAnimalMovementArrival(movementArrival) {
@@ -71,24 +76,132 @@ function onAnimalMovementArrival(movementArrival) {
   movementArrival.animal.location = movementArrival.arrivalField;
 
      // save the animal
-  var ar = getAssetRegistry('com.ibm.concerto.mozart.Animal');
-  ar.update(movementArrival.animal);
+  return getAssetRegistry('com.ibm.concerto.mozart.Animal')
+  .then(function(ar) {
+    return ar.update(movementArrival.animal);
+  })
+  .then(function() {
+    // remove the animal from the incoming animals
+    // of the 'to' business
+    if (!movementArrival.to.incomingAnimals) {
+      throw new Error('Incoming business should have incomingAnimals on AnimalMovementArrival.');
+    }
 
-     // remove the animal from the incoming animals
-     // of the 'to' business
-  if (!movementArrival.to.incomingAnimals) {
-    throw new Error('Incoming business should have incomingAnimals on AnimalMovementArrival.');
-  }
-
-  movementArrival.to.incomingAnimals = movementArrival.to.incomingAnimals
-      .filter(function(animal) {
-        return animal.animalId !== movementArrival.animal.animalId;
-      });
+    movementArrival.to.incomingAnimals = movementArrival.to.incomingAnimals
+    .filter(function(animal) {
+      return animal.animalId !== movementArrival.animal.animalId;
+    });
 
       // save the business
-  var br = getAssetRegistry('com.ibm.concerto.mozart.Business');
-  br.update(movementArrival.to);
+    return getAssetRegistry('com.ibm.concerto.mozart.Business');
+  })
+  .then(function(br) {
+    return br.update(movementArrival.to);
+  });
 }
+
+/**
+ *
+ * @param {com.ibm.concerto.mozart.SetupDemo} setupDemo
+ * @transaction
+ */
+function setupDemo(setupDemo) {
+  var factory = getFactory();
+  var NS = 'com.ibm.concerto.mozart';
+
+  var farmers = [
+    factory.newResource(NS, 'Farmer', 'FARMER_1'),
+    factory.newResource(NS, 'Farmer', 'FARMER_2')
+  ];
+
+  var businesses = [
+    factory.newResource(NS, 'Business', 'BUSINESS_1'),
+    factory.newResource(NS, 'Business', 'BUSINESS_2')
+  ];
+
+  var fields = [
+    factory.newResource(NS, 'Field','FIELD_1'),
+    factory.newResource(NS, 'Field','FIELD_2'),
+    factory.newResource(NS, 'Field','FIELD_3'),
+    factory.newResource(NS, 'Field','FIELD_4')
+  ];
+
+  var animals = [
+    factory.newResource(NS, 'Animal', 'ANIMAL_1'),
+    factory.newResource(NS, 'Animal', 'ANIMAL_2'),
+    factory.newResource(NS, 'Animal', 'ANIMAL_3'),
+    factory.newResource(NS, 'Animal', 'ANIMAL_4'),
+    factory.newResource(NS, 'Animal', 'ANIMAL_5'),
+    factory.newResource(NS, 'Animal', 'ANIMAL_6'),
+    factory.newResource(NS, 'Animal', 'ANIMAL_7'),
+    factory.newResource(NS, 'Animal', 'ANIMAL_8')
+  ];
+  return getParticipantRegistry(NS + '.Regulator')
+  .then(function(regulatorRegistry) {
+    var regulator = factory.newResource(NS, 'Regulator', 'REGULATOR');
+    regulator.email = 'REGULATOR';
+    regulator.firstName = 'Ronnie';
+    regulator.lastName = 'Regulator';
+    return regulatorRegistry.addAll([regulator]);
+  })
+  .then(function() {
+    return getParticipantRegistry(NS + '.Farmer');
+  })
+  .then(function(farmerRegistry) {
+    farmers.forEach(function(farmer) {
+      var sbi = 'BUSINESS_' + farmer.getIdentifier().split('_')[1];
+      farmer.firstName = farmer.getIdentifier();
+      farmer.lastName = '';
+      farmer.address1 = 'Address1';
+      farmer.address2 = 'Address2';
+      farmer.county = 'County';
+      farmer.postcode = 'PO57C0D3';
+    });
+    return farmerRegistry.addAll(farmers);
+  })
+  .then(function() {
+    return getAssetRegistry(NS + '.Business');
+  })
+  .then(function(businessRegistry) {
+    businesses.forEach(function(business, index) {
+      var cph = 'FIELD_' + (index + 1);
+      var farmer = 'FARMER_' + (index + 1);
+      business.address1 = 'Address1';
+      business.address2 = 'Address2';
+      business.county = 'County';
+      business.postcode = 'PO57C0D3';
+      business.owner = factory.newRelationship(NS, 'Farmer', farmer);
+    });
+
+    return businessRegistry.addAll(businesses);
+  })
+  .then(function() {
+    return getAssetRegistry(NS + '.Field');
+  })
+  .then(function(fieldRegistry) {
+    fields.forEach(function(field, index) {
+      var business = 'BUSINESS_' + (((index + 1) % 2) + 1);
+      field.name = 'FIELD_' + (index + 1);
+      field.business = factory.newRelationship(NS, 'Business', business);
+    });
+    return fieldRegistry.addAll(fields);
+  })
+  .then(function() {
+    return getAssetRegistry(NS + '.Animal');
+  })
+  .then(function(animalRegistry) {
+    animals.forEach(function(animal, index) {
+      var field = 'FIELD_' + (((index + 1) % 2) + 1);
+      var farmer = 'FARMER_' + (((index + 1) % 2) + 1);
+      animal.species = 'SHEEP_GOAT';
+      animal.movementStatus = 'IN_FIELD';
+      animal.productionType = 'MEAT';
+      animal.location = factory.newRelationship(NS, 'Field', field);
+      animal.owner = factory.newRelationship(NS, 'Farmer', farmer);
+    });
+    return animalRegistry.addAll(animals);
+  });
+} 
 
 /**
  * Get the Animals, but do not resolve contained relationships
