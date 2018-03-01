a669a13c57ebf061985058cc661a6e648407c7a1
Andrew Hurt <31732124+awjh-ibm@users.noreply.github.com>
2018-03-01 12:42:31 +0000

    added the options field to a PlaceOrderEvent (#149)
    Signed-off-by: awjh-ibm <andrew.hurt1@ibm.com>

--- packages/vehicle-manufacture-network/lib/script.js
+++ packages/vehicle-manufacture-network/lib/script.js
@@ -40,6 +40,7 @@ function placeOrder(orderRequest) {
       var placeOrderEvent = factory.newEvent(namespace, 'PlaceOrderEvent');
       placeOrderEvent.orderId = order.orderId;
       placeOrderEvent.vehicleDetails = order.vehicleDetails;
+      placeOrderEvent.options = order.options;
       placeOrderEvent.orderer = order.orderer;
       emit(placeOrderEvent);
     });
